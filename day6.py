a2be029d0e5f16a18aad4b07d659c4c15cb4aae6
marywantsme <526bor@gmail.com>
2026-03-22 15:09:31 +0200

    clean up day6: remove unused os import, simplify save function

--- day6.py
+++ day6.py
@@ -1,10 +1,8 @@
-import os
 import random
 import datetime
 WORDS = ["sova", "sobaka", "papa", "dog", "house", "paper", "sun", "light", "car", "slon"]
 
 def generate_password():
-    password = ""
     word = random.choice(WORDS)
     symbol = random.choice(("!", "@", "#", "$", "%"))
     number = random.randint(10, 99)
@@ -23,26 +21,20 @@ def load_passwords():
     return passwords
 
 def save_passwords(passwords):
-    os.path.exists("passwords.txt")
-    if os.path.isfile("passwords.txt"):
-        with open("passwords.txt", "a") as file:
-         for password in passwords:
-            file.write(password + "\n")
-    else: 
-        with open("passwords.txt", "w") as file:
-         for password in passwords:
+    with open("passwords.txt", "a") as file:
+        for password in passwords:
             file.write(password + "\n")
 
 name = input("Введите имя: ")
 n = int(input("Сколько паролей сгенерировать? "))
 
-datatime = load_passwords()
-print("Загружено паролей: " + str(len(datatime)))
+existing_passwords = load_passwords()
+print("Загружено паролей: " + str(len(existing_passwords)))
 
-save_passwords([name + " |" +" (" + str(datetime.date.today()) + ") | " + generate_password() for _ in range(n)])
-print("Пароли сохранены.")  
+new_passwords = [generate_password() for _ in range(n)]
 
-for _ in range(n):
-    print(generate_password())
+save_passwords([name + " | " + str(datetime.date.today()) + " | " + p for p in new_passwords])
+for p in new_passwords:
+    print(p)
 
 
